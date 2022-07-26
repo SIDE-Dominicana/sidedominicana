a35c57ec08d41be772ce96189363051885954a65
CrisDaniel25 <cristopherdanielfigueroa@gmail.com>
2022-07-25 21:01:11 -0400

    [UPDATE] - Avance Pagina Web

--- client-admin/src/index.js
+++ client-admin/src/index.js
@@ -13,8 +13,6 @@ import Auth from "layouts/Auth.js";
 // views without layouts
 
 import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
-import Index from "views/Index.js";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -23,9 +21,7 @@ ReactDOM.render(
       <Route path="/admin" component={Admin} />
       <Route path="/auth" component={Auth} />
       {/* add routes without layouts */}
-      <Route path="/landing" exact component={Landing} />
-      <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
+      <Route path="/" exact component={Landing} />
       {/* add redirect for first page */}
       <Redirect from="*" to="/" />
     </Switch>
--- client-admin/src/views/Landing.js
+++ client-admin/src/views/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import brand from '../assets/img/IMG-20210302-WA0016.jpg';
 
 // components
 
@@ -16,7 +17,7 @@ export default function Landing() {
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
               backgroundImage:
-                "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+                `url(${brand})`,
             }}
           >
             <span
