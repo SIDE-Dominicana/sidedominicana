4c8197ff974b6c3ac0f3d26aa2baf5d69dd36c42
Jeiny Dayhana Figueroa Rosario <66802775+jfigueroa2106@users.noreply.github.com>
2022-11-18 16:03:12 -0400

    Model
    Add row model products. Price of the products insert the database.

--- src/model/products.model.js
+++ src/model/products.model.js
@@ -5,8 +5,9 @@ function model(sequelize) {
         productsId: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
         name: { type: DataTypes.STRING, allowNull: false },
         description: { type: DataTypes.STRING, allowNull: false },
+        price: { type: DataTypes.INTEGER, allowNull: false },
         creationDate: { type: DataTypes.DATE, allowNull: false },
-       UpdateDate: { type: DataTypes.DATE, allowNull: false },
+        UpdateDate: { type: DataTypes.DATE, allowNull: false },
         status: { type: DataTypes.BOOLEAN, allowNull: false }
     };
 
